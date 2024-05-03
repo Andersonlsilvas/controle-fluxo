ae909d630f3530864526d81faab9dae9726c198e
Anderson Santos <andersonlsilvas86@gmail.com>
2024-05-03 11:31:33 -0300

    Programa finalizado

--- src/application/Contador.java
+++ src/application/Contador.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
 public class Contador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o Primeiro Paramentro: ");
+		int parametroUm = sc.nextInt();
+		System.out.print("Digite o Segundo Parametro: ");
+		int parametroDois = sc.nextInt();
+		
+		try {
+			contar(parametroUm,parametroDois);
+		}catch(ParametrosInvalidosException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		sc.close();
 	}
-
+		
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if(parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");		
+		}
+		System.out.println();
+		int contagem = parametroDois - parametroUm;
+		for(int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o Numero " + i );
+		}
+		
+	}
 }
